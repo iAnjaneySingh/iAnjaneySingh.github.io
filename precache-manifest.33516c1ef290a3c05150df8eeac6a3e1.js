697418e9d1561605f9dfee1fda11a7b7dca6676c
Anjaney Singh <asinghbits5@gmail.com>
2020-09-09 23:29:02 +0530

    Deployed my website on my special repo

---
+++ precache-manifest.33516c1ef290a3c05150df8eeac6a3e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753ff87ca8299dde12ddc7a304348a65",
+    "url": "/iAnjaneySingh/iAnjaneySingh.github.io/index.html"
+  },
+  {
+    "revision": "cca20a12c704ca634f84",
+    "url": "/iAnjaneySingh/iAnjaneySingh.github.io/static/css/main.cfc4f5b1.chunk.css"
+  },
+  {
+    "revision": "28b06c8bd506254cabfa",
+    "url": "/iAnjaneySingh/iAnjaneySingh.github.io/static/js/2.25134c0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iAnjaneySingh/iAnjaneySingh.github.io/static/js/2.25134c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca20a12c704ca634f84",
+    "url": "/iAnjaneySingh/iAnjaneySingh.github.io/static/js/main.2f6665ce.chunk.js"
+  },
+  {
+    "revision": "fdb346b2273d76843a94",
+    "url": "/iAnjaneySingh/iAnjaneySingh.github.io/static/js/runtime-main.0f87f993.js"
+  },
+  {
+    "revision": "faff043942c03e39c81af574e5b1262e",
+    "url": "/iAnjaneySingh/iAnjaneySingh.github.io/static/media/5.faff0439.jpg"
+  }
+]);
